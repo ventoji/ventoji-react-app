16b64dfc5edc0b713256257d652aadc555946fba
Jorge Vento <vento.ji@gmail.com>
2019-06-12 07:58:16 +0200

    Updates

---
+++ precache-manifest.499fc167cac4bb9836c84cc0253aae61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d467be6cfeea632d2053af6252675bd4",
+    "url": "/ventoji-react-app/index.html"
+  },
+  {
+    "revision": "49b850d6c4bf07b2c4ea",
+    "url": "/ventoji-react-app/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/ventoji-react-app/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "49b850d6c4bf07b2c4ea",
+    "url": "/ventoji-react-app/static/js/main.09cc7c2c.chunk.js"
+  },
+  {
+    "revision": "e22be561b8448f0a916f",
+    "url": "/ventoji-react-app/static/js/runtime~main.be7004a1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ventoji-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
